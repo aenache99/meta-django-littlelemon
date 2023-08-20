414d80d3baf6dede18efc5716aa35884aa0ea4e6
Alex Enache <55555550+aenache99@users.noreply.github.com>
2023-08-20 22:32:32 +0300

    Update views.py

--- workplace/littlelemon/restaurant/views.py
+++ workplace/littlelemon/restaurant/views.py
@@ -2,39 +2,34 @@
 from django.shortcuts import render
 from .forms import BookingForm
 from .models import Menu
+from django.http import HttpResponse
+
 
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
-
+    return render(request, 'index.html')
 
 def about(request):
-    return render(request, "about.html")
-
+    return render(request, 'about.html')
 
 def book(request):
     form = BookingForm()
-    if request.method == "POST":
+    if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
             form.save()
-    context = {"form": form}
-    return render(request, "book.html", context)
+    context = {'form':form}
+    return render(request, 'book.html', context)
 
-
-# Add your code here to create new views
 def menu(request):
     menu_data = Menu.objects.all()
     main_data = {"menu": menu_data}
+    return render(request, 'menu.html', {"menu": main_data})
 
-    return render(request, "menu.html", main_data)
-
-
-def display_menu_items(request, pk=None):
-    if pk:
-        menu_item = Menu.objects.get(pk=pk)
-    else:
-        menu_item = ""
-
-    return render(request, "menu_item.html", {"menu_item": menu_item})
+def display_menu_item(request, pk=None): 
+    if pk: 
+        menu_item = Menu.objects.get(pk=pk) 
+    else: 
+        menu_item = "" 
+    return render(request, 'menu_item.html', {"menu_item": menu_item}) 
